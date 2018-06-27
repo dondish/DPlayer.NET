f5df57ef0174e917139f7855e3764f51bab4995d
dondish <odedshapira1@gmail.com>
2018-06-27 15:50:01 +0300

    Checking that GitHub works

---
+++ DPlayer.NET/Format/AudioFormat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DPlayer.NET.Format
+{
+    abstract class AudioFormat
+    {
+        public readonly int channels;
+
+        public readonly int samplerate;
+
+        public readonly int samplecount;
+        
+        AudioFormat(int channels, int samplerate, int samplecount)
+        {
+            this.channels = channels;
+            this.
+        }
+
+    }
+}
